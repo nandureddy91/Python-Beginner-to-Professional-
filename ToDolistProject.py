ae3eb3d62c9c57bff759ffcd4cbeb39691ef429f
kethireddyk <143539993+Kethitreddyk@users.noreply.github.com>
2024-01-22 11:31:45 -0600

    checked a logic

--- ToDolistProject.py
+++ ToDolistProject.py
@@ -1,6 +1,8 @@
 Todo=[]
 while True:
     user_Text = input("Enter a ToDo:")
-    Todo.append(user_Text.capitalize())
+    Todo.append(user_Text.TI())
     print(Todo)
     
+
+
--- experements.py
+++ experements.py
@@ -1,2 +1,7 @@
-while True:
-    print("hei")
+# while True:
+#     print("hei")
+passwd = input("Enter the password:")
+while passwd != "Hello!":
+    print("The password is Incorrect .PLease enter the correct password")
+    passwd = input("enter the password:\n")
+print("welcome to TEH World")
