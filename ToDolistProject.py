f2fd92767918b59267deaa77176801854ff82ade
kethireddyk <143539993+Kethitreddyk@users.noreply.github.com>
2024-01-22 14:06:11 -0600

    added capital letters in the begining

--- ToDolistProject.py
+++ ToDolistProject.py
@@ -4,19 +4,21 @@ while True:
     match user_Text:
         case "add":
             Todo= input("enter the todo:")
+            Todo.title()
             Todos.append(Todo)
-        case "show":
+        case "show"|"display":
             for item in Todos:
+                item=item.title()
                 print(item)
-        case "delete":
+        case "delete"|"remove":
             print("Which todo do you want to delete")
             for index, element in enumerate(Todos, start=0):
-                print(f"{index}. {element}")
+                print(f"{index}. {element}".title())
             user_delete=int(input("enter the number of the todo you want to delete: \n"))
             if 0<user_delete<len(Todos):
                 delete_Todo=Todos.pop(user_delete)
             else:
-                print("You have chosen the wrong selection")
+                print("You have chosen the wrong selection there is nothing to delete")
             
         case "exit":
             print("The Todo List is:")
