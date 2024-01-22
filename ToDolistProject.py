17f2754b70ac5bbba9f046db5386ec7aeef197a5
kethireddyk <143539993+Kethitreddyk@users.noreply.github.com>
2024-01-22 13:50:28 -0600

    added delete option flexibility

--- ToDolistProject.py
+++ ToDolistProject.py
@@ -1,15 +1,25 @@
 Todos=[]
 while True:
-    user_Text = input("choose and type add or delete or exit:")
+    user_Text = input("choose and type add or delete or show or  exit:")
     match user_Text:
         case "add":
             Todo= input("enter the todo:")
             Todos.append(Todo)
-            print(Todos)
+        case "show":
+            for item in Todos:
+                print(item)
         case "delete":
-            Todo=Todos.pop()
+            print("Which todo do you want to delete")
+            for index, element in enumerate(Todos, start=0):
+                print(f"{index}. {element}")
+            user_delete=int(input("enter the number of the todo you want to delete: \n"))
+            if 0<user_delete<len(Todos):
+                delete_Todo=Todos.pop(user_delete)
+            else:
+                print("You have chosen the wrong selection")
+            
         case "exit":
-            print("the todo list is:")
+            print("The Todo List is:")
             break    
 print(Todos)
 
