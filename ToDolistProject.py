dbbbf0361ac138e45d75e0b508f24a73264bd85f
kethireddyk <143539993+Kethitreddyk@users.noreply.github.com>
2024-01-22 14:41:54 -0600

    added edit feature to the ToDo list app

--- ToDolistProject.py
+++ ToDolistProject.py
@@ -18,8 +18,20 @@ while True:
             if 0<user_delete<len(Todos):
                 delete_Todo=Todos.pop(user_delete)
             else:
-                print("You have chosen the wrong selection there is nothing to delete")
+                print("You have chosen the wrong selection there is nothing to delete")  
+                  
+        case "Edit":
+            print("Which todo do you want to delete")
+            for index, element in enumerate(Todos, start=0):
+                print(f"{index}. {element}".title())
+            user_edit=int(input("enter the number of the todo you want to delete: \n"))
+            if 0<user_edit<len(Todos):
+                edit_todo=input("enter new edited todo")
+                Todos[user_edit]=edit_todo
             
+            else:
+                print("You have chosen the wrong selection there is nothing to delete: \n")    
+                     
         case "exit":
             print("The Todo List is:")
             break    
