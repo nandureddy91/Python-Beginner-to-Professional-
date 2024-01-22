d91607e981ec81597ae3219ff93f274317d17c15
kethireddyk <143539993+Kethitreddyk@users.noreply.github.com>
2024-01-22 13:28:22 -0600

    added more funcationlity to the app

--- ToDolistProject.py
+++ ToDolistProject.py
@@ -1,8 +1,16 @@
-Todo=[]
+Todos=[]
 while True:
-    user_Text = input("Enter a ToDo:")
-    Todo.append(user_Text.TI())
-    print(Todo)
-    
+    user_Text = input("choose and type add or delete or exit:")
+    match user_Text:
+        case "add":
+            Todo= input("enter the todo:")
+            Todos.append(Todo)
+            print(Todos)
+        case "delete":
+            Todo=Todos.pop()
+        case "exit":
+            print("the todo list is:")
+            break    
+print(Todos)
 
 
--- experements.py
+++ experements.py
@@ -1,7 +1,9 @@
-# while True:
-#     print("hei")
-passwd = input("Enter the password:")
-while passwd != "Hello!":
-    print("The password is Incorrect .PLease enter the correct password")
-    passwd = input("enter the password:\n")
-print("welcome to TEH World")
+name = input("Enter your Name:")
+while True:
+    print(name.capitalize())
+     
+# passwd = input("Enter the password:")
+# while passwd != "Hello!":
+#     print("The password is Incorrect .PLease enter the correct password")
+#     passwd = input("enter the password:\n")
+# print("welcome to TEH World")
